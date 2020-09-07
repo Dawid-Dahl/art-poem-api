d7c5f46bd0bbb1f8d03866d7687be040149d9995
Dawid Dahl <dawiddahl@gmail.com>
2020-09-07 17:03:29 +0200

    prepared project for deployment

--- src/api/middleware/verifyXToken.ts
+++ src/api/middleware/verifyXToken.ts
@@ -1,17 +1,12 @@
-import path from "path";
-import fs from "fs";
 import {Request, Response, NextFunction} from "express-serve-static-core";
 import jwt from "jsonwebtoken";
 import {removeBearerFromTokenHeader, extractPayloadFromBase64JWT} from "../utils/utils";
-
-const PUB_KEY_PATH = path.join(__dirname, "..", "cryptography", "id_rsa_pub.pem");
-const PUB_KEY = fs.readFileSync(PUB_KEY_PATH, "utf8");
 
 const verifyXToken = (req: Request, res: Response, next: NextFunction) => {
 	const tokenWithoutBearer = removeBearerFromTokenHeader(req.get("x-token"));
 
 	if (tokenWithoutBearer) {
-		jwt.verify(tokenWithoutBearer, PUB_KEY, err => {
+		jwt.verify(tokenWithoutBearer, process.env.PUB_KEY as string, err => {
 			if (err) {
 				res.status(401).json({
 					success: false,
